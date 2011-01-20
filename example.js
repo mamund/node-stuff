d7f649f13b0369dffb5dba04d238f84584db7db8
Mike Amundsen <mamund@yahoo.com>
2011-01-20 18:20:53 -0500

    first commit

---
+++ example.js
@@ -0,0 +1,18 @@
+// 2011-01-20 (mca)
+
+var http = require('http');
+
+var g = {};
+g.port = 8124;
+g.ip = '127.0.0.1';
+
+http.createServer(hello).listen(g.port, g.ip);
+console.log('Server running at http://'+g.ip+':'+g.port+'/');  
+
+function hello(req, res) 
+{
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World, mamund!\n');
+}
+  
+
